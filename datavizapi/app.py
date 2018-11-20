a40bbb94b0ed1fb65e4bb13e8377b89c70189420
Abhinav <abhinavkumarfzd@gmail.com>
2018-11-20 12:35:54 -0500

    formatting

--- datavizapi/app.py
+++ datavizapi/app.py
@@ -1,40 +1,45 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
 
-from psd import (small_sample_dataset,total_data_in_time_range,
-	average_power_in_time_range,average_power_in_time_range_in_freq_range)
+from psd import (small_sample_dataset, total_data_in_time_range,
+                 average_power_in_time_range,
+                 average_power_in_time_range_in_freq_range)
 from sensor_metadata import get_sensor_metadata
 
+app = Flask(__name__)
 
-app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return jsonify(go_to="/api/dataset/small")
+    return jsonify(go_to="/api/dataset/small")
+
 
 @app.route('/api/sample/<count>')
 def get_small_dataset(count=1):
-	count=int(request.args.get("count",count))
-	count=min(count,2000)
-	return jsonify(small_sample_dataset(count))
+    count = int(request.args.get("count", count))
+    count = min(count, 2000)
+    return jsonify(small_sample_dataset(count))
+
 
 @app.route('/api/avg_power/<start>/<end>/<f1>/<f2>')
-def get_energy_history_in_time_range_in_freq_range(start,end,f1,f2):
-	return jsonify(average_power_in_time_range_in_freq_range(start,end,f1,f2))
+def get_energy_history_in_time_range_in_freq_range(start, end, f1, f2):
+    return jsonify(
+        average_power_in_time_range_in_freq_range(start, end, f1, f2))
+
 
 @app.route('/api/all/<start>/<end>')
-def psd_data_in_time_range(start,end):
-	return jsonify(total_data_in_time_range(start,end))
+def psd_data_in_time_range(start, end):
+    return jsonify(total_data_in_time_range(start, end))
+
 
 @app.route('/api/avg_power/<start>/<end>')
-def psd_data_avg_power_in_time_range(start,end):
-	return jsonify(average_power_in_time_range(start,end))
+def psd_data_avg_power_in_time_range(start, end):
+    return jsonify(average_power_in_time_range(start, end))
+
 
 @app.route('/api/sensor_info/<daq_id>')
 def get_sensor_info(daq_id):
-	return jsonify(get_sensor_metadata(daq_id))
+    return jsonify(get_sensor_metadata(daq_id))
 
 
-
-
-if __name__=='__main__':
-	app.run(debug=True,host='0.0.0.0',port=8000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
--- datavizapi/db.py
+++ datavizapi/db.py
@@ -1,47 +1,52 @@
 import MySQLdb
 from config import config
 
-db=MySQLdb.connect(user=config['DATABASE_USER'],
-    passwd=config['DATABASE_PASSWORD'],db=config['DATABASE_NAME'])
+db = MySQLdb.connect(
+    user=config['DATABASE_USER'],
+    passwd=config['DATABASE_PASSWORD'],
+    db=config['DATABASE_NAME'])
+
 
 def connect():
-	global db
-	db=MySQLdb.connect(user=config['DATABASE_USER'],
-	    passwd=config['DATABASE_PASSWORD'],db=config['DATABASE_NAME'])
+    global db
+    db = MySQLdb.connect(
+        user=config['DATABASE_USER'],
+        passwd=config['DATABASE_PASSWORD'],
+        db=config['DATABASE_NAME'])
+
 
 def get_db():
-	global db
-	return db
+    global db
+    return db
+
 
 def query(func):
-	def wrapper(*args, **kwargs):
-		try:
-			return func(*args,**kwargs)
-		except MySQLdb.OperationalError:
-			connect()
-			return func(*args,**kwargs)
-	return wrapper
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except MySQLdb.OperationalError:
+            connect()
+            return func(*args, **kwargs)
+
+    return wrapper
 
 
 @query
 def execute_one(q_string, params=()):
-	cursor=db.cursor()
-	cursor.execute(q_string, params)
-	resp=cursor.fetchall()
-	cursor.close()
-	return resp
+    cursor = db.cursor()
+    cursor.execute(q_string, params)
+    resp = cursor.fetchall()
+    cursor.close()
+    return resp
+
 
 @query
 def execute_many(q_string, params=[]):
-	cursor=db.cursor()
-	cursor.executemany(q_string,params)
-	resp=cursor.fetchall()
-	cursor.close()
-	return resp
-
-
-
-
+    cursor = db.cursor()
+    cursor.executemany(q_string, params)
+    resp = cursor.fetchall()
+    cursor.close()
+    return resp
 
 
 # def close_db(e=None):
@@ -56,4 +61,3 @@ def execute_many(q_string, params=[]):
 
 # def init_app(app):
 # 	app.teardown_appcontext(close_db)
-
