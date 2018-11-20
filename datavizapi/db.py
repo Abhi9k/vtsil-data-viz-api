70290f4de44e9d9712772d8ab78b4fed35148ba0
Abhinav <abhinavkumarfzd@gmail.com>
2018-11-20 12:28:52 -0500

    modified db logic and added api

--- datavizapi/db.py
+++ datavizapi/db.py
@@ -1,21 +1,59 @@
-from flask import current_app, g
-from flask_mysqldb import MySQL
+import MySQLdb
+from config import config
+
+db=MySQLdb.connect(user=config['DATABASE_USER'],
+    passwd=config['DATABASE_PASSWORD'],db=config['DATABASE_NAME'])
+
+def connect():
+	global db
+	db=MySQLdb.connect(user=config['DATABASE_USER'],
+	    passwd=config['DATABASE_PASSWORD'],db=config['DATABASE_NAME'])
 
 def get_db():
-	if db not in g:
-		g.db = MySQL(current_app)
-	return g.db
+	global db
+	return db
 
-def close_db():
-	db = g.pop('db', None)
-	if db is not None:
-		db.close()
+def query(func):
+	def wrapper(*args, **kwargs):
+		try:
+			return func(*args,**kwargs)
+		except MySQLdb.OperationalError:
+			connect()
+			return func(*args,**kwargs)
+	return wrapper
 
-def init_db():
-	db = get_db()
-	with current_app.open_resource('schema.sql') as f:
-		pass
 
-def init_app(app):
-	app.teardown_appcontext(close_db)
+@query
+def execute_one(q_string, params=()):
+	cursor=db.cursor()
+	cursor.execute(q_string, params)
+	resp=cursor.fetchall()
+	cursor.close()
+	return resp
+
+@query
+def execute_many(q_string, params=[]):
+	cursor=db.cursor()
+	cursor.executemany(q_string,params)
+	resp=cursor.fetchall()
+	cursor.close()
+	return resp
+
+
+
+
+
+
+# def close_db(e=None):
+# 	db = g.pop('db', None)
+# 	if db is not None:
+# 		db.close()
+
+# def init_db():
+# 	db = get_db()
+# 	with current_app.open_resource('schema.sql') as f:
+# 		pass
+
+# def init_app(app):
+# 	app.teardown_appcontext(close_db)
 
---
+++ datavizapi/utils.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+
+SENSOR_DATE_TIME_FORMAT='%Y-%m-%d %H:%M:%S'
+FILENAME_DATE_FORMAT='%Y-%m-%d_%H_%M_%S'
+
+def parseDate(d, format=FILENAME_DATE_FORMAT):
+    return datetime.strptime(d, format)
+
+def formatDate(d, format=SENSOR_DATE_TIME_FORMAT):
+    return datetime.strftime(d,format)
+
+def generateTimestamp(start_time, second,
+		format=SENSOR_DATE_TIME_FORMAT):
+    days=(second/(60*60))/24
+    hours=(second-days*60*60*24)/(60*60)
+    minutes=(second-days*60*60*24-hours*60*60)/60
+    seconds=(second-days*60*60*24-hours*60*60-minutes*60)
+    timestamp = formatDate(
+    	start_time + timedelta(days=days,seconds=seconds,
+    		minutes=minutes,hours=hours),format)
+
+    return timestamp
