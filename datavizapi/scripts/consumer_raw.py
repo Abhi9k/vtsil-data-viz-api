128836e918944939764adeff455359754528c1f1
Abhinav <abhinavkumarfzd@gmail.com>
2019-04-25 13:49:29 -0400

    renamed consumer_psd to consumer_raw

--- datavizapi/scripts/consumer_raw.py
+++ datavizapi/scripts/consumer_raw.py
@@ -1,6 +1,5 @@
 from confluent_kafka import Consumer
-import sys
-sys.path.append('/home/vtsil/vtsil-data-viz-api')
+# from scipy import signal, integrate
 import datavizapi.cassandra_operations as db_op
 from datavizapi import AppConfig
 import json
@@ -10,36 +9,40 @@ kafka_config = config['kafka']
 
 c = Consumer({
     'bootstrap.servers': ",".join(kafka_config['servers']),
-    'group.id': kafka_config['consumer_raw']['group'],
-    'auto.offset.reset': kafka_config['consumer_raw']['offset']})
+    'group.id': kafka_config['consumer_psd']['group'],
+    'auto.offset.reset': kafka_config['consumer_psd']['offset']})
+
+c.subscribe([kafka_config['consumer_psd']['topic']])
+
+# def delivery_report(err, msg):
+#     """ Called once for each message produced to indicate delivery result.
+#         Triggered by poll() or flush(). """
+#     if err is not None:
+#         print('Message delivery failed: {}'.format(err))
+#     else:
+#         print('Message delivered to {} [{}]'.format(
+#             msg.topic(), msg.partition()))
 
 
-def getSensorIds():
-    sensor_info_objects = db_op.getSensorInfoAll()
-    sids = []
-    for obj in sensor_info_objects:
-        sids.append(str(obj.id))
-    return sids
+# def power_spectrum(input_arr, sampling_f=256.0,
+#                    scaling='density', window='hann',
+#                    window_size=256):
+#     window = signal.get_window(window, len(input_arr))
+#     return signal.welch(
+#         input_arr, fs=sampling_f, scaling=scaling, window=window)
 
 
-def startConsumer(topics):
-    c.subscribe(topics)
-    while True:
-        msg = c.poll(1)
-        if msg is None:
-            continue
-        if msg.error():
-            print("Consumer error: {}".format(msg.error()))
-            continue
-        msg_parsed = json.loads(msg.value())
-        sid = msg_parsed['sid']
-        db_op.insertSensorData(sid, msg['ts'], msg['d'])
-
-
-if __name__ == '__main__':
-    start = int(sys.argv[1])
-    end = int(sys.argv[2])
-    sids = getSensorIds()
-    sids = sids[start:end]
-    topics = map(lambda x: 'rawData_' + x, sids)
-    startConsumer(topics)
+while True:
+    msg = c.poll(1)
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+    msg = json.loads(msg.value())
+    # freqs, power = power_spectrum(msg['d'], sampling_f=float(msg['f']))
+    # average_power = integrate.simps(power)
+    # sid = getDaqId(msg['daq_name'])
+    sid = msg['sid']
+    db_op.insertSensorData(sid, msg['ts'], msg['d'])
+    # db_op.insertPSD(sid, average_power, power, msg['ts'])
